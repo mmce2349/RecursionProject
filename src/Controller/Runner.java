64c1d747347f91c1d330c7e8d30f6f2def039dfd
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-06-01 07:36:51 -0600

    final commit

--- src/Controller/Runner.java
+++ src/Controller/Runner.java
@@ -1,5 +1,14 @@
 package Controller;
 
-public class Runner {
+import Controller.Controller;
 
+public class Runner 
+{
+	public static void main(String [] args)
+	{
+		Controller myApp = new Controller();
+		myApp.start();
+		
+		
+	}
 }
